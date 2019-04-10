1726aa0b8e3ef6671900879c1e9674812bdd99ae
MaxHearnden <MaxHearnden@users.noreply.github.com>
2019-04-10 14:56:03 +0100

    added basic shuffler
    this shuffler only choeses who's hand the card should be in. DO NOT USE THIS FOR GAMES WHERE ORDER MATTERS E.G. Poker

---
+++ shuffler.py
@@ -0,0 +1,7 @@
+import random
+while True:
+    deck=list(range(0,int(input("no of cards"))))
+    random.shuffle(deck)
+    players=int(input("no of players"))
+    for i in deck:
+        input(i%players+1)

acb8a773aa8842528f65f79232463073b5044688
Max Hearnden <max@hearnden.org.uk>
2021-06-06 23:35:01 +0100

    add an index to output

--- shuffler.py
+++ shuffler.py
@@ -3,5 +3,5 @@ while True:
     deck=list(range(0,int(input("no of cards"))))
     random.shuffle(deck)
     players=int(input("no of players"))
-    for i in deck:
-        input(i%players+1)
+    for i in range(len(deck)):
+        input(i,deck[i]%players+1)
